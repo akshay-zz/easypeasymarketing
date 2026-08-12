e8ae6be48920e345cb8724d197bec7f50f07f79d
akshay-zz <akshaykumar01010@gmail.com>
2026-08-13 00:06:07 +0530

    Update environment variables for improved clarity and organization

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 export const EnvVars = {
-  SITE_NAME: 'My SaaS Startup',
-  OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
-  URL: 'https://next-saas-starter-ashy.vercel.app/',
+  SITE_NAME: 'Easy Peasy Marketing',
+  OG_IMAGES_URL: 'https://easypeasymarketing.in/og-images/',
+  URL: 'https://easypeasymarketing.in/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
