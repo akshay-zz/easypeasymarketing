e49ca4f8324cea1e4e8b570093758d4d25533e8b
akshay-zz <akshaykumar01010@gmail.com>
2026-08-14 19:49:58 +0530

    Add simpler version

--- env.ts
+++
@@ -1,6 +0,0 @@
-export const EnvVars = {
-  SITE_NAME: 'Easy Peasy Marketing',
-  OG_IMAGES_URL: 'https://easypeasymarketing.in/og-images/',
-  URL: 'https://easypeasymarketing.in/',
-  MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
-};
--- types.ts
+++
@@ -1,9 +0,0 @@
-export type SingleNavItem = { title: string; href: string; outlined?: boolean };
-
-export type NavItems = SingleNavItem[];
-
-export type NonNullableChildren<T> = { [P in keyof T]: Required<NonNullable<T[P]>> };
-
-export type NonNullableChildrenDeep<T> = {
-  [P in keyof T]-?: NonNullableChildrenDeep<NonNullable<T[P]>>;
-};
