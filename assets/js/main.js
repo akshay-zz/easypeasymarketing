06ad9e1a11d2cf8019002b7c0c0a5b595fc39894
akshay-zz <akshaykumar01010@gmail.com>
2026-08-16 00:51:58 +0530

    Update template name and URL in README and CSS/JS files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
-* Template Name: FlexStart
-* Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
+* Template Name: Easy Peasy Marketing
+* Template URL: https://bootstrapmade.com/Easy Peasy Marketing-bootstrap-startup-template/
 * Updated: Nov 01 2024 with Bootstrap v5.3.3
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
